9e56a834ca55f4c86bf2ea0765630d6db8d3497d
Shivam Kumar <shivamkumar27052003@gmail.com>
2025-04-10 00:18:13 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main class="flex justify-center gap-4 flex-col min-h-screen ">
-      <h1 class="text-3xl font-bold text-center, text-left">Welcome to my app</h1>
+      <h1 class="text-3xl font-bold text-center, text-left">my app</h1>
 
 
     </main>
