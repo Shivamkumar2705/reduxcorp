76eaf4bfee7d1203fc8a2a4a2e33941047bb4125
Shivam Kumar <shivamkumar27052003@gmail.com>
2025-04-10 01:45:05 +0530

    commit-added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main class="flex justify-center gap-4 flex-col min-h-screen ">
-      <h1 class="text-3xl font-bold text-center, text-left">my app is  building apppp</h1>
+      <h1 class="text-3xl font-bold text-center, text-left">my app is yyh  building apppp</h1>
     </main>
   );
 }
