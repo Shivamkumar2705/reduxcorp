fb82f12b718c53e1755ed34330fa179f87213354
The-YashGaur <gauryash0904@gmail.com>
2025-04-10 16:10:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <>
+      <div className="border-b border-gray-200">
+        <Navbar />
+      </div>
+      <main>
+        <Home />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-white shadow-md p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl font-bold text-blue-600">
+          Add_Logo
+        </Link>
+        <ul className="flex gap-6 text-gray-700 font-medium">
+          <li>
+            <Link to="/" className="hover:text-blue-600">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects" className="hover:text-red-500">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-blue-600">About</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-blue-600">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
